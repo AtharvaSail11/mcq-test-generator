156e5c65c194c6540a1c08267fdbbd5229b7f62b
atharvasail11 <atharvasail11@gmail.com>
2025-11-09 14:55:15 +0530

    Made some UI related changes to ResultDisplay.jsx

--- src/components/mcqPageComponents/ResultDisplay.jsx
+++ src/components/mcqPageComponents/ResultDisplay.jsx
@@ -5,9 +5,13 @@ import { Clock } from "lucide-react";
 const ResultDisplay=({correctAnswer,incorrectAnswer,selectedAnswer})=>{
 
     return(
-        <div className="flex flex-col justify-center w-full p-5">
-            <p>Correct:{correctAnswer}</p>
-            <p>Incorrect:{incorrectAnswer}</p>
+        <div className="flex flex-col items-center w-full p-5">
+            <div className="flex flex-col items-center w-1/2 h-max">
+            <p className="font-semibold text-2xl text-green-500">Correct:{correctAnswer}</p>
+            <p className="font-semibold text-2xl text-red-500">Incorrect:{incorrectAnswer}</p>
+             </div>
+
+             <div className="flex flex-col justify-center w-1/2 h-max">
             {mcqQuestions.map((questionData,index)=>(
                 <div className="flex flex-col justify-center w-full mt-5">
                 <div>
@@ -16,7 +20,7 @@ const ResultDisplay=({correctAnswer,incorrectAnswer,selectedAnswer})=>{
             <div className="flex flex-col">
                 <div className="flex flex-col">
                     {questionData.options.map((option,index2)=>(
-                    <button className={`relative w-max border border-black ${(option === questionData.correctAnswer && 'bg-green-500 text-white border-green-500')||(selectedAnswer[index] === index2 && 'bg-red-500 text-white border-red-500')} font-semibold rounded-xl px-4 py-1 m-0.5 cursor-pointer`} key={index2}>
+                    <button className={`flex justify-start relative w-full border border-black ${(option === questionData.correctAnswer && 'bg-green-500 text-white border-green-500')||(selectedAnswer[index] === index2 && 'bg-red-500 text-white border-red-500')} font-semibold rounded-xl px-4 py-1 m-0.5 cursor-pointer`} key={index2}>
                         <p>{option}</p>
                     </button>
                     ))}
@@ -24,7 +28,7 @@ const ResultDisplay=({correctAnswer,incorrectAnswer,selectedAnswer})=>{
             </div>
             </div>
             ))}
-            
+            </div>
         </div>
     )
 }
