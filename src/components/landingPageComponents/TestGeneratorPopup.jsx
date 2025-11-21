2432dbca405b54053bd0a6258c0052fca1059f16
atharvasail11 <atharvasail11@gmail.com>
2025-11-21 17:17:42 +0530

    Added Function to send question generation request to Mcq generator server

--- src/components/landingPageComponents/TestGeneratorPopup.jsx
+++ src/components/landingPageComponents/TestGeneratorPopup.jsx
@@ -1,15 +1,45 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 
 const TestGeneratorPopup=({setTestGeneratorPopup})=>{
-    const [file,setFile]=useState(null);
+    const fileRef=useRef(null);
+    const [fileName,setFileName]=useState('Upload File');
+    const [testDuration,setTestDuration]=useState(null);
+    const [numOfQuestions,setNumOfQuestions]=useState(null);
+
+
+
+    const handleSelect=(e)=>{
+        e.preventDefault();
+        console.log('selectedDuration:',testDuration);
+        setTestDuration(e.target.value);
+    }
 
     const handleFileChange=(e)=>{
-        e.preventDefault();
-        const selectedFile=e.target.files[0];
-        if(selectedFile){
-            setFile(selectedFile.name);
+        const files=e.target.files;
+        console.log("selectedFile",files[0].name);
+        if(files.length > 0){
+            setFileName(files[0].name);
+        }else{
+            setFileName('Upload File')
         }
+    }
+
+    const handleRequestSending=async()=>{
+        const file=fileRef.current.files[0];
+
+        const formData=new FormData();
+        formData.append('docFile',file);
+        formData.append('numberOfQuestions',10);
+        formData.append('testDuration',15);
+        const response=await fetch('http://localhost:3000/api/generateQuestions',{
+            method:'POST',
+            body:formData
+        });
+
+        const data=await response.json();
+
+        console.log('response from server:',data);
     }
 
     return(
@@ -23,17 +53,17 @@ const TestGeneratorPopup=({setTestGeneratorPopup})=>{
             </div>
             <div className=" flex flex-col w-full border-2 border-dashed border-gray-400 rounded">
                     <div className="flex flex-col gap-0.5 justify-center items-center w-full">
-                        <p className="relative mt-2 font-bold">{file?file:"Upload File"}</p>
+                        <p className="relative mt-2 font-bold">{fileName}</p>
                         <p className="relative mt-2">{"Upload a .txt, .pdf, or .docx file"}</p>
                         <label className="relative font-semibold min-w-[84px] max-w-[480px] bg-gray-200 hover:bg-gray-300 cursor-pointer text-black rounded-xl px-4 py-2 m-2" htmlFor="file-input">Choose a file</label>
-                        <input style={{display:"none"}} id="file-input" name="file-input" type="file" accept="application/pdf, .docx" onChange={handleFileChange}/> 
+                        <input style={{display:"none"}} id="file-input" name="file-input" ref={fileRef} type="file" accept="application/pdf, .docx" onChange={handleFileChange}/> 
                     </div>
 
             </div>
             <div className="flex w-full gap-4">
                     <div className="flex flex-col w-1/2 justify-between">
                         <label htmlFor="required-time">Test Duration</label>
-                        <select className="border border-gray-300 rounded-md" id="required-time">
+                        <select className="border border-gray-300 rounded-md" id="required-time" onChange={handleSelect}>
                             <option value="">Select</option>
                             <option value="15" >15 min</option>
                             <option value="30" >30 min</option>
@@ -45,7 +75,7 @@ const TestGeneratorPopup=({setTestGeneratorPopup})=>{
                         <input className="border border-gray-300 rounded-md" placeholder="eg:5" type="number" name="questions-num" id="questions-num" />
                     </div>
             </div>
-            <button className="relative w-full bg-blue-500 hover:bg-blue-700 font-semibold text-white rounded-xl px-1.5 py-2 m-2">Generate Test</button>
+            <button className="relative w-full bg-blue-500 hover:bg-blue-700 font-semibold text-white rounded-xl px-1.5 py-2 m-2" onClick={handleRequestSending}>Generate Test</button>
         </div>
     )
 }
